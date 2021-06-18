6ce5aa8f6fb4b6f647083a33c9ac84b073db17bc
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-18 11:29:12 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+<style>
+header{
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 150;
+background: 	#A8A8A8;
+}
+.logo{
+position: relative;
+top: 35;
+left: 80;
+width: 70;
+height: 70;
+background: gray;
+}
+.title{
+position: absolute;
+top: 15;
+left: 160;
+width: 160;
+height: 70;
+background: ;
+color: gray;
+}
+.title{
+position: absolute;
+top: 15;
+right: 160;
+width: 160;
+height: 70;
+background: ;
+}
+</style>
+
+<header>
+<div class="logo"></div>
+<div class="title"><h2>Title</h2>Store</div>
+<div class="title"><h2>Title</h2>Store</div>
+<div class="number"></div>
+
+</header>
