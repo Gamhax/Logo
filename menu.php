ba0014f965c791cdfa0993159fde9cb0910d1c0b
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-18 11:31:01 +0300

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,21 @@
+<style>
+.menu{
+position: absolute;
+top: 150;
+left: 0;
+width: 100%;
+height: 50;
+background: gray;
+}
+.menu a{
+text-decoration: none;
+color: white;
+margin: 5px;
+
+}
+.menu a:hover {
+    text-decoration: underline;
+   }
+</style>
+<center>
+<div class="menu"><p><a href="#">Главная &#160;&brvbar;&#160;</a><a href="#">Каталог&#160;&brvbar;&#160;</a><a href="#">Доставка і оплата&#160;&brvbar;&#160;</a><a href="#">Прайс-лист&#160;&brvbar;&#160;</a><a href="#">Контакти&#160;&brvbar;&#160;</a></div>
