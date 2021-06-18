641e78f717b2d6835c62a4335e4e9acbc05c9dbd
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-18 11:27:29 +0300

    Create content.php

---
+++ content.php
@@ -0,0 +1,35 @@
+<style>
+.content{
+position: absolute;
+top: 520;
+left: 0;
+width: 100%;
+height: 600;
+background: ;
+}
+.content article{
+position: relative;
+top: 100;
+left: -300;
+width: 300;
+height: 500;
+background: gray;
+color: white;
+}
+.content section{
+position: relative;
+top: -400;
+left: 200;
+width: 600;
+height: 500;
+}
+</style>
+<div class="content">
+<article>
+<h1>Новини</h1>
+</article>
+<section>
+<h1 style="text-align: left;">Компанія Піксель Плюс</h1>
+<p style="text-align: left;">Компанія Піксель плюс надає свої послуги</p>
+</section>
+</div>
