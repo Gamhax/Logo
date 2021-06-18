602e393efc0b1f77a4e6959b0c5ae1ba5ad40732
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-18 11:28:11 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<style>
+footer{
+position: absolute;
+top: 1350;
+left: 0;
+width: 100%;
+height: 250;
+background: gray;
+}
+footer div{
+float: left;
+width: 25%;
+height: 200;
+margin: 3%;
+color: white;
+}
+footer  a:hover {
+text-decoration: underline;
+}
+</style>
+<footer>
+<div class="left">
+<p style="text-align: left;">&#169;  Компанія 2010-2021</p><p style="text-align: left;">info@info.com</p>
+</div>
+<div class="cent">
+<p style="text-align: left;"><p style="text-align: left;><a href="#">Главная</a><p style="text-align: left;><a href="#">Каталог</a><p style="text-align: left;><a href="#">Доставка і оплата</a><p style="text-align: left;><a href="#">Прайс-лист</a><p style="text-align: left;><a href="#">Контакти</a></p>
+</div>
+<div class="right">
+<p style="text-align: left;">Разработка сайта <p style="text-align: left;">Пиксель Плюс</p>
+</div>
+</footer>
