77badcf69b2e709c2a67fd5dfd0821baa8a174be
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-22 07:09:25 +0300

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ top: 0;
 left: 0;
 width: 100%;
 height: 150;
-background: 	#A8A8A8;
+background:;
 }
 .logo{
 position: relative;
@@ -23,21 +23,26 @@ width: 160;
 height: 70;
 background: ;
 color: gray;
+font-family: Arial;
+font-size: 12px;
+
 }
-.title{
+.number{
 position: absolute;
 top: 15;
 right: 160;
-width: 160;
-height: 70;
+width: 190;
+height: 100;
 background: ;
+color: gray;
+font-family: Arial;
+font-size: 20px;
+
 }
 </style>
-
 <header>
 <div class="logo"></div>
-<div class="title"><h2>Title</h2>Store</div>
-<div class="title"><h2>Title</h2>Store</div>
-<div class="number"></div>
+<div class="title"><h2>Название компании</h2>The best store</div>
+<div class="number"><p>+380-xxx-xxx-x</p><p>+380-xxx-xxx-x</p></div>
 
 </header>
