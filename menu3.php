7690e1f1c11de28b4e9760acf15e669743f5b24a
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-22 08:42:02 +0300

    Create menu3.php

---
+++ menu3.php
@@ -0,0 +1,21 @@
+<style>
+.menu{
+position: absolute;
+top: 150;
+left: 0;
+width: 100%;
+height: 50;
+background: gray;
+}
+.menu a{
+text-decoration: none;
+color: white;
+margin: 5px;
+
+}
+.menu a:hover {
+    text-decoration: underline;
+   }
+</style>
+<center>
+<div cla
