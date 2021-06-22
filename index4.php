a0aebd6b4a5723835a484d9a0c9707cd969427f7
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-22 11:34:08 +0300

    Create index4.php

---
+++ index4.php
@@ -0,0 +1,154 @@
+<style>
+nav{
+position: fixed;
+top: 0;
+left: 0;
+width: 100%;
+height: 45;
+background: white;
+box-shadow: 0 0 10px gray;
+}
+.menu{
+position: fixed;
+top: 10;
+right: 50;
+width: 60%;
+height: 40;
+}
+.logo{
+position: fixed;
+top: 5;
+left: 150;
+width: 35;
+height: 35;
+border-radius: 100%;
+background: #00CED1;
+}
+.menu a{
+margin: 5px;
+text-decoration: none;
+color: gray;
+}
+.former{
+position: absolute;
+top: 50;
+left: 10%;
+width: 80%;
+height: 500;
+background: #F0F0F0;
+}
+.allformer{
+position: absolute;
+top: 50;
+left: 0%;
+width: 100%;
+height: 550;
+background: #F0F0F0;
+}
+.leftform{
+float: left;
+width: 50%;
+height: 300;
+background: ;
+margin-top: 100;
+}
+.rightform{
+float: right;
+width: 50%;
+height: 500;
+background: ;
+}
+.rightform input{
+float: left;
+width: 100%;
+height: 25;
+border-radius: 4px;
+}
+.dewsite{
+position: absolute;
+top: 600;
+left: 20%;
+width: 60%;
+height: 400;
+background: ;
+}
+.dewsite1{
+position: absolute;
+top: 1000;
+left: 0%;
+width: 100%;
+height: 300;
+background: #6666FF;
+}
+.dewsite2{
+position: absolute;
+top: 1300;
+left: 0%;
+width: 100%;
+height: 400;
+background: ;
+}
+.dewsite3{
+position: absolute;
+top: 1700;
+left: 0%;
+width: 100%;
+height: 500;
+background: #F0F0F0;
+}
+.dewsite4{
+position: absolute;
+top: 2300;
+left: 0%;
+width: 100%;
+height: 600;
+background: ;
+}
+</style>
+<body>
+<nav>
+<dov class="logo"></div>
+<div class="menu">
+<a href="">Головна</a><a href="">Програма курсу</a><a href="">Відгуки про курс</a><a href="">Відео</a><a href="">Ментори</a><a style="background: #00CED1; border-radius: 15px; padding: 4px;" href="">Подати заявку</a>
+</div>
+</nav>
+<div class="allformer">
+<div class="former">
+<div class="leftform"><h1>Lorem ipsum dolor sit amet!</h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+<div class="rightform">
+<form>
+<p>Ім`я</p>
+<input type="text" placeholder="Ім`я" required>
+<p>Прізвище</p>
+<p><input type="text" placeholder="Прізвище" required>
+<p>Мене цікавить</p>
+<input type="text" placeholder="Мене цікавить" required>
+<p>Номер телефону</p>
+<input type="number" placeholder="Номер телефону" required>
+<p>Електронна адреса</p>
+<input type="email" placeholder="Електронна адреса" required>
+<p style="float: left;"><input type="checkbox" placeholder="Політика" style="margin-top: 10px;">Політика конфіденційності</p>
+<button type="submit" style="margin-left: 30%; width: 250px; height: 50px; color: white; background: #00CED1; border: none; border-radius: 20px;">Зареєструватись зараз</button>
+</form>
+</div>
+</div>
+</div>
+<div class="dewsite"><center><h1>Розробка сайтів</h1></center>
+<div class="l" style="width: 30%; height: 400; float: left; padding: 10px;"><h2>Lorem ipsum dolor sit amet!</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p></div>
+<div class="c" style="width: 30%; height: 400; float: left; padding: 10px;"><h2>Lorem ipsum dolor sit amet!</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p></div>
+<div class="r" style="width: 30%; height: 400; float: left; padding: 10px;"><h2>Lorem ipsum dolor sit amet!</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor</p></div>
+</div>
+<div class="dewsite1"></div>
+<div class="dewsite2">
+<center><h2>Що кажуть студенти наших курсів</h2><p style="width: 60%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor!</p></center>
+</div>
+<div class="dewsite3">
+<center><h2>Відео</h2><p style="width: 60%;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut!</p></center>
+<center><iframe width="560" height="315" src="https://www.youtube.com/embed/v0Fi_nkUH_4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</center>
+</div>
+<div class="dewsite4"><center><h1>Наші ментори</h1><p style="color: blue;">Віктор</p>
+<div style="float: left; width:50%; height: 300px; background: ;" class=""><img src="" style="float: right;  width:70%; height: 200px;"></div><p style="width: 35%; float: left;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</center></div>
+
+</body>					
