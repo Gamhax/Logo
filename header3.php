f3b8727f58507ed8cd4e78b5043f1b89bceb2dfe
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-22 08:41:20 +0300

    Create header3.php

---
+++ header3.php
@@ -0,0 +1,48 @@
+<style>
+header{
+position: absolute;
+top: 0;
+left: 0;
+width: 100%;
+height: 150;
+background:;
+}
+.logo{
+position: relative;
+top: 35;
+left: 80;
+width: 70;
+height: 70;
+background: gray;
+}
+.title{
+position: absolute;
+top: 15;
+left: 160;
+width: 160;
+height: 70;
+background: ;
+color: gray;
+font-family: Arial;
+font-size: 12px;
+
+}
+.number{
+position: absolute;
+top: 15;
+right: 160;
+width: 190;
+height: 100;
+background: ;
+color: gray;
+font-family: Arial;
+font-size: 20px;
+
+}
+</style>
+<header>
+<div class="logo"></div>
+<div class="title"><h2>Название компании</h2>The best store</div>
+<div class="number"><p>+380-xxx-xxx-x</p><p>+380-xxx-xxx-x</p></div>
+
+</header>
