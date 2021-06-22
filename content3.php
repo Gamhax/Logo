b34af0ac6c2ec1e2d5184c98bf5306b0a68955b4
Serg <81491331+Gamhax@users.noreply.github.com>
2021-06-22 08:39:45 +0300

    Create content3.php

---
+++ content3.php
@@ -0,0 +1,92 @@
+<style>
+.content{
+position: absolute;
+top: 520;
+left: 15%;
+width: 65%;
+height: 600;
+background: ;
+}
+.content article{
+position: relative;
+top: 10;
+left: -370;
+width: 130;
+height: 500;
+background: ;
+color: black;
+}
+.content section{
+position: relative;
+top: -530;
+left: 200;
+width: 800;
+height: 500;
+}
+.commodity{
+float: left;
+width: 200;
+height: 310;
+margin: 12;
+background: gray;
+}
+.commodity div{
+width: 180;
+height: 180;
+margin: 10;
+background: white;
+}
+.undercontent{
+position: absolute;
+top: 1350;
+left: 20%;
+width: 60%;
+height: 250;
+background: ;
+}
+.left{
+float: left;
+width: 50%;
+height: 250;
+background: ;
+}
+.right{
+float: right;
+width: 50%;
+height: 250;
+background: ;
+}
+</style>
+<center>
+<div class="content">
+<p style="text-align: left; font-size: 10px;">Головна / Каталог / Побутова техніка</p>
+<article>
+<p style="text-align: left; padding: 5px; font-size: 15px;">Автомобілі</br>Побутова техніка</p>
+<ul style="text-align: left; font-size: 13px;">
+<li>Холодильники</li>
+<li>Посудомийка</li>
+<li>Чайник</li>
+</ul>
+<p style="text-align: left; padding: 5px; font-size: 15px;">Мобільна техніка</br>Одяг</br>Нерухомість</br>Комп'ютери</p>
+</article>
+<section>
+<div class="commodity"><div class="images"></div><p style="font-size: 15px;">Дуже топовий товар нашого магазину!</p><h2 style="text-align: left; padding: 10px;">2000 грн</h2></div>
+<div class="commodity"><div class="images"></div><p style="font-size: 15px;">Дуже топовий товар нашого магазину!</p><h2 style="text-align: left; padding: 10px;">2000 грн</h2></div>
+<div class="commodity"><div class="images"></div><p style="font-size: 15px;">Дуже топовий товар нашого магазину!</p><h2 style="text-align: left; padding: 10px;">2000 грн</h2></div>
+<div class="commodity"><div class="images"></div><p style="font-size: 15px;">Дуже топовий товар нашого магазину!</p><h2 style="text-align: left; padding: 10px;">2000 грн</h2></div>
+<div class="commodity"><div class="images"></div><p style="font-size: 15px;">Дуже топовий товар нашого магазину!</p><h2 style="text-align: left; padding: 10px;">2000 грн</h2></div>
+<p style="position: relative; top: 700; left: -500;">1  2  3  4  5 6  7</p>
+
+</section>
+
+</div>
+<div class="undercontent">
+<div class="left">
+<p style="text-align: left; padding: 15px; font-size: 15px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="right">
+<p style="text-align: left; padding: 15px; font-size: 15px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+</div>
+</div>
+</center>
